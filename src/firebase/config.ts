9703bede36e616f0a26ecff9a403c441aa883bf7
Ariadna Ramos torres <ariadnaramostorres@gmail.com>
2026-04-27 08:24:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4310204158-2c3c6",
+  "appId": "1:247508784962:web:fb8592d8c4d86719f49adb",
+  "apiKey": "AIzaSyAXjERiKeV12dCjPn5WiFwNvulL9ui_fIE",
+  "authDomain": "studio-4310204158-2c3c6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "247508784962"
+};
